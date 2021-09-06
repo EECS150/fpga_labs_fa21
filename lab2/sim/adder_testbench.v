bb22db30b8e67a6a11c1b85795e332c79efda630
Class Account <eecs151-tae@c111-2.eecs.berkeley.edu>
2021-09-06 16:44:36 -0700

    fix some vcs issues

--- lab2/sim/adder_testbench.v
+++ lab2/sim/adder_testbench.v
@@ -26,17 +26,17 @@ module adder_testbench();
         a = 14'd1;
         b = 14'd1;
         #(2);
-        assert(sum == 'd2);
+        assert(sum == 15'd2);
 
         a = 14'd0;
         b = 14'd1;
         #(2);
-        assert(sum == 'd1) else $display("ERROR: Expected sum to be 1, actual value: %d", sum);
+        assert(sum == 15'd1) else $display("ERROR: Expected sum to be 1, actual value: %d", sum);
 
         a = 14'd10;
         b = 14'd10;
         #(2);
-        if (sum != 'd20) begin
+        if (sum != 15'd20) begin
             $error("Expected sum to be 20, a: %d, b: %d, actual value: %d", a, b, sum);
             $fatal(1);
         end
